ff3b3a9c86c1aaf4314ffcd2f45b5e809f685db6
Kartik Dogra <kartikdogra2@gmail.com>
2022-10-13 20:21:33 +0100

    final changes passes 2 out of 3 test

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from pickle import TRUE
+
+
 def primes(number_of_primes):
     list = []
+
+    # loop until len(list) = number
+    # is i prime
+    # loop through numbers from 2 to i
+    # if i % n==0 then i is not prime
+    #i is not prime move to next number;
+    i=2
+    prime=True
+    while len(list)<number_of_primes:
+        prime=True
+        j=i/2
+        while j>1:
+            if(i%j==0):
+                prime=False
+                i=i+1
+            j=j-1
+        if prime:
+            list.append(i)
+            i=i+1
     return list
--- primes_test.py
+++ primes_test.py
@@ -5,19 +5,16 @@ from primes import primes
 
 def test_1_primes():
     actual = primes(1)
-    print(actual)
     expected = [2]
     assert actual == expected
 
 def test_2_primes():
     actual = primes(2)
-    print(actual)
     expected = [2, 3]
     assert actual == expected
 
 def test_20_primes():
     actual = primes(20)
-    print(actual)
     expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
                 53, 59, 61, 67, 71]
     assert actual == expected
